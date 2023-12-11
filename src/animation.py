ee01ac0cbff1fa184fb5158ff3abfe8d3bb0a132
Jeanne Grenet <jeanne.grent@gmail.com>
2023-12-11 15:35:29 +0100

    add space

--- src/animation.py
+++ src/animation.py
@@ -13,6 +13,7 @@ class AnimateSprite(pygame.sprite.Sprite):
             'left' : self.get_images(48),
             'right' : self.get_images(96)     
             }
+        
         self.speed=3
     #Load one image of the player    
     def get_image(self,x,y) : 
