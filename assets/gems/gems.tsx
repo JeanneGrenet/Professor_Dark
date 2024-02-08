11134c20589bee6583c51308f7d1404e6a942015
Jeanne <jeanne.grenet@gmail.com>
2024-02-08 13:15:30 +0100

    gems asset, git ignore and delete pycache

---
+++ assets/gems/gems.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="gems" tilewidth="16" tileheight="16" tilecount="2304" columns="48">
+ <image source="33076_VXACEtoMV_Dungeon_B_png-par-whtdragon, Kadokawa.png" width="768" height="768"/>
+</tileset>
